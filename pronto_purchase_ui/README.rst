3db0d3cd32db40d428993a196e3a9a21378d0f53
Juan Pablo Garza <juanp@juanpgarza.com>
2022-10-01 07:42:10 -0300

    [12.0][IMP] pronto_purchase_ui: Se agrega a la OC las columnas cantidad en mano, cantidad prevista, precio de costo y precio facturado por proveedor

---
+++ pronto_purchase_ui/README.rst
@@ -0,0 +1,9 @@
+=================
+Purchase UI
+=================
+
+Detalle de cambios:
+
+#. Marca de fecha prevista re-confirmada para las ordenes de compra
+#. Se agrega a la OC las columnas cantidad en mano, cantidad prevista, precio de costo y precio facturado por proveedor.
+
---
+++ pronto_purchase_ui/models/purchase_order_line.py
@@ -0,0 +1,24 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, fields
+from odoo.exceptions import ValidationError
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    qty_available = fields.Float("Cantidad a mano", related='product_id.qty_available')
+    virtual_available = fields.Float("Cantidad prevista", related='product_id.virtual_available')
+
+    vendor_invoice_price_unit = fields.Float("Precio Unitario Facturado", compute="_compute_invoice_price_unit")
+    cost_price_unit = fields.Char("Precio de costo", compute="_compute_cost_price_unit")
+
+    @api.one
+    @api.depends('order_id.invoice_ids.invoice_line_ids.price_unit')
+    def _compute_invoice_price_unit(self):
+        self.vendor_invoice_price_unit = self.order_id.invoice_ids.invoice_line_ids.filtered(lambda x: x.purchase_line_id.id == self.id).price_unit 
+
+    @api.one
+    def _compute_invoice_price_unit(self):
+        cost_pricelist_id = self.company_id.product_pricelist_cost_id
+        self.cost_price_unit = cost_pricelist_id.item_ids.filtered(lambda x: x.product_tmpl_id.id == self.product_id.product_tmpl_id.id).price
