632f6d68408bc14a48e89d29c2fbb224feb55003
Juan Pablo Garza <juanp@juanpgarza.com>
2022-11-30 18:27:50 -0300

    [15.0][ADD] pronto_sale

---
+++ pronto_sale/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "pronto_sale_ui",
+    "summary": "Mejoras solicitadas por el cliente (Ventas)",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Sale",
+    "website": "https://github.com/juanpgarza/pronto-custom",
+    "author": "juanpgarza",
+    "depends": [
+        "sale",
+        ],
+    "data": [
+        ],
+    "installable": True,
+}
