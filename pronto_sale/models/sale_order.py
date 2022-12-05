289c89cb33d515294388edfad1b5882271166257
Juan Pablo Garza <juanp@juanpgarza.com>
2022-12-05 09:57:27 -0300

    [15.0][X] muevo método valid_quotation() a sale_tier_validation_ux

--- pronto_sale/models/sale_order.py
+++ pronto_sale/models/sale_order.py
@@ -4,13 +4,3 @@ from odoo.exceptions import ValidationError,UserError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # lo uso en una validación de nivel de tipo Fórmula.
-    # el código para controlar la fecha de vencimiento es: not rec.valid_quotation()
-    def valid_quotation(self):
-        for rec in self:
-            delta = rec.validity_date - fields.Date.context_today(self)
-
-            if delta.days >= 0:
-                return True
-            else:
-                return False
