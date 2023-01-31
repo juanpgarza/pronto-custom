cc4b8da1ae72c2ad0a384bbab45dce64c5152454
Juan Pablo Garza <juanp@juanpgarza.com>
2023-01-31 11:11:50 -0300

    [12.0][FIX] pronto_purchase_ui: - que no oculte el precio facturado cuando se confirma

--- pronto_purchase_ui/__manifest__.py
+++ pronto_purchase_ui/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "pronto_purchase_ui",
     "summary": "Mejoras a la UI solicitadas por el cliente",
-    "version": "12.0.2.0.0",
+    "version": "12.0.2.1.0",
     "category": "Product",
     "website": "https://github.com/juanpgarza/pronto-custom",
     "author": "juanpgarza",
