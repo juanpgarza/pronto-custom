41829368f407f2b7ee98e6fb54826adaed30066b
Juan Pablo Garza <juanp@juanpgarza.com>
2023-10-09 09:25:26 -0300

    [15.0][ADD] website_sale_checkout_extra_fields y website_sale_observations

---
+++ website_sale_checkout_extra_fields/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Website Sale Checkout Extra Fields',
+    'summary': 'Adds the possibility of capturing new fields at checkout',
+    'category': 'Website',
+    'version': '15.0.1.0.0',
+    'author': 'Trey (www.trey.es)',
+    'website': 'https://www.trey.es',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'website',
+        'website_sale',
+    ],
+    'data': [
+        # 'views/website.xml',
+        'views/website_sale.xml',
+    ],
+    'installable': True,
+}
---
+++ website_sale_observations/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Website Sale Observations',
+    'summary': 'Allow introduce customers observations in web orders',
+    'category': 'Website',
+    'version': '15.0.1.0.0',
+    'author': 'Trey (www.trey.es)',
+    'website': 'https://www.trey.es',
+    'license': 'AGPL-3',
+    'depends': [
+        'website_sale',
+        'website_sale_checkout_extra_fields',
+    ],
+    'data': [
+        'views/website_sale.xml',
+    ],
+    'installable': True,
+}
