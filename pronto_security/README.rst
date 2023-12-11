88f0d88e08fb3f27b96217a03cebd36b44951662
Juan Pablo Garza <juanp@juanpgarza.com>
2023-12-11 19:31:24 -0300

    [15.0][ADD] pronto_security

---
+++ pronto_security/README.rst
@@ -0,0 +1,9 @@
+=================
+Pronto Security
+=================
+
+Detalle de cambios:
+
+#. Los usuarios del grupo "stock: Usuario vendedor" pueden navegar desde un pedido de ventas a la orden de entrega
+#. 
+
---
+++ pronto_security/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "pronto_security",
+    "summary": "Mejoras solicitadas por el cliente",
+    "version": "15.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/juanpgarza/pronto-custom",
+    "author": "juanpgarza",
+    "license": "AGPL-3",
+    "depends": [
+            "sale",
+            "pronto",
+        ],
+    "data": [
+            'security/ir.model.access.csv',
+            'views/sale_order_views.xml',
+        ],
+    "installable": True,
+}
