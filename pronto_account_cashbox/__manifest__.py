39736086e8dbcb9bea553e1cb215981670c1ab5e
Juan Pablo Garza <juanp@juanpgarza.com>
2023-12-27 11:05:36 -0300

    [15.0][FIX] pronto_account_cashbox: solo lectura el saldo de la sesión anterior

--- pronto_account_cashbox/__manifest__.py
+++ pronto_account_cashbox/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "pronto_account_cashbox",
     "summary": "",
-    "version": "15.0.3.2.0",
+    "version": "15.0.3.3.0",
     "category": "Accounting",
     "website": "https://github.com/juanpgarza/pronto-custom",
     "author": "juanpgarza",
