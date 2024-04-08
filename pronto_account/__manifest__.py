818229d9011c6d2a8e512e6f0f1bec45c8cc5c47
Juan Pablo Garza <juanp@juanpgarza.com>
2024-04-08 12:25:06 -0300

    [15.0][IMP] pronto_account

--- pronto_account/__manifest__.py
+++ pronto_account/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "pronto_account",
     "summary": "Mejoras solicitadas por el cliente",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "category": "Sale",
     "website": "https://github.com/juanpgarza/pronto-custom",
     "author": "juanpgarza",
@@ -13,6 +13,7 @@
             "account",
         ],
     "data": [
+        'data/pronto_data.xml',
         ],
     "installable": True,
 }
