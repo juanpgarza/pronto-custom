2e23eee2f98b90b4d9535a3ace49db0013e979fd
Juan Pablo Garza <juanp@juanpgarza.com>
2024-04-22 19:42:52 -0300

    [15.0][IMP] pronto_sale: Agrego campo 'Precio unitario con descuento' a las líneas de presupuestos

--- pronto_sale/README.rst
+++ pronto_sale/README.rst
@@ -8,4 +8,5 @@ Detalle de cambios:
 #. Que el botón "Actualizar Precios" se muestre siempre (aunque no se haya cambiado la tarifa)
 #. Que obligue a informar la plantilla de presupuestos antes de informar los productos
 #. pronto_sale: Que tome la descripción de venta de las líneas de promociones desde el campo 'descripción de venta' del producto
+#. Agrego campo 'Precio unitario con descuento' a las líneas de presupuestos
 
--- pronto_sale/models/sale_order_line.py
+++ pronto_sale/models/sale_order_line.py
@@ -1,10 +1,20 @@
-from odoo import models, api
+from odoo import models, api, fields
 from odoo.exceptions import ValidationError
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    precio_unitario_con_descuento = fields.Float('Precio unitario con descuento', compute="_compute_precio_unitario_con_descuento")
+
     @api.onchange('product_id')
     def onchange_product_id(self):
         if not self.order_id.sale_order_template_id:
             raise ValidationError("Antes de informar los productos debe informar la plantilla")
+
+    @api.depends('product_id', 'price_unit', 'discount')
+    def _compute_precio_unitario_con_descuento(self):
+        for line in self:
+            if line.discount > 0:
+                line.precio_unitario_con_descuento = line.price_unit * (1 - (1/line.discount))
+            else:
+                line.precio_unitario_con_descuento = line.price_unit
