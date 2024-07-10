79a728cc9dcfd07437c35e80171ccaa860c2800e
Juan Pablo Garza <juanp@juanpgarza.com>
2024-07-10 10:23:58 -0300

    [15.0][FIX] pronto_sale: Agrego campo 'Precio unitario con descuento' a las líneas de presupuestos

--- pronto_sale/models/sale_order_line.py
+++ pronto_sale/models/sale_order_line.py
@@ -14,7 +14,4 @@ class SaleOrderLine(models.Model):
     @api.depends('product_id', 'price_unit', 'discount')
     def _compute_precio_unitario_con_descuento(self):
         for line in self:
-            if line.discount > 0:
-                line.precio_unitario_con_descuento = line.price_unit * (1 - (1/line.discount))
-            else:
-                line.precio_unitario_con_descuento = line.price_unit
+            line.precio_unitario_con_descuento = line.price_unit * (1 - (line.discount/100))
