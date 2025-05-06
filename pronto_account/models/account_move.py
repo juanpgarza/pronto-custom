7eac98ad5388afc2a48d7dd16f17e8b3fe51e4c5
Juan Pablo Garza <juanp@juanpgarza.com>
2025-05-06 17:39:20 -0300

    [17.0][IMP] pronto_account: Bloqueo pasar a borrador comprobante fiscal validado

--- pronto_account/models/account_move.py
+++ pronto_account/models/account_move.py
@@ -61,3 +61,9 @@ class AccountMove(models.Model):
         # estos campos no los va a tener en cuenta para la validación
         res.append('needed_terms_dirty')
         return res
+
+    def button_draft(self):
+        res = super(AccountMove,self).button_draft()
+        for rec in self:
+            if rec.move_type in ('out_invoice','out_refund') and rec.journal_id.l10n_latam_use_documents and not self.env.user.has_group('pronto_account.group_cancelar_comprobante_fiscal'):                
+                raise ValidationError("No es posible cambiar a borrador a un comprobante fiscal validado contra AFIP.")
