7bfb9f5e00b2afbd9361727f2fa5b6c4459b0503
Juan Pablo Garza <juanp@juanpgarza.com>
2025-07-28 08:21:11 -0300

    MIG pronto_website_sale

--- pronto_website_sale/__manifest__.py
+++ pronto_website_sale/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "pronto_website_sale",
     "summary": "Mejoras solicitadas por el cliente",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sale",
     "website": "https://github.com/juanpgarza/pronto-custom",
     "author": "juanpgarza",
@@ -17,5 +17,5 @@
             # 'security/pronto_sale_security.xml',
             'views/website_sale_template.xml',
         ],
-    "installable": False,
+    "installable": True,
 }
